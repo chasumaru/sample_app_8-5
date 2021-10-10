a6d4f170f24125f20660f2cd7534f7e193eb93c2
chasu <ptj.chasu@gmail.com>
2021-10-10 10:47:21 +0000

    hard to resolve git probrem

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,9 +12,8 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在ログインしているユーザーを返す（いる場合）
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    # ユーザーIDにユーザーIDのセッションを代入した結果）ユーザーIDのセッションが存在すれば
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
@@ -26,17 +25,28 @@ module SessionsHelper
     end
   end
 
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
 
-    # 永続的セッションを破棄する
-    def forget(user)
-      user.forget
-      cookies.delete(:user_id)
-      cookies.delete(:remember_token)
-    end
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   # 現在のユーザーをログアウトする
   def log_out
@@ -45,6 +55,14 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
-  
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
