386df209f480c7881d98bc66d64d6012b239a623
chasu <ptj.chasu@gmail.com>
2021-10-11 09:35:12 +0000

    make User mailer account-activation before 11.2.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ User.create!(name:  "Example User",
     password:              "foobar",
     password_confirmation: "foobar",
     admin: true)
+    activated: true
+    activated_at: Time.zone.now
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -14,4 +16,6 @@ User.create!(name:  name,
       email: email,
       password:              password,
       password_confirmation: password)
+      activated: true
+      activated_at: Time.zone.now
 end
